954564bbe2185c2ea38a5a0d346b7add7883c059
thisisamal <amalpullan4@gmail.com>
2023-07-29 20:40:12 +0530

    done

--- Backend/urls.py
+++ Backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('data/', include('console.urls')),
+    path('console/', include('console.urls')),
 ]
